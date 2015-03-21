c6b264687cdaaa052bd1d3915ce748d92debf7d7
think-t <think.t2@gmail.com>
2015-03-21 21:10:28 +0900

    changed to code for centos.

--- jdk/recipes/oraclejdk.rb
+++ jdk/recipes/oraclejdk.rb
@@ -14,33 +14,36 @@ oraclejdk_major   = oraclejdk_version.split('u')[0]
 oraclejdk_minor   = oraclejdk_version.split('u')[1]
 jdk_version       = "jdk1.#{oraclejdk_major}.0_#{oraclejdk_minor}"
 
-if node['kernel']['machine'] == 'i686' then
-  oraclejdk_rpm = "jdk-#{oraclejdk_version}-linux-i586.rpm"
-elsif node['kernel']['machine'] == 'x86_64' then
-  oraclejdk_rpm = "jdk-#{oraclejdk_version}-linux-x64.rpm"
-end
+case node['platform']
+when "centos" then
+  if node['kernel']['machine'] == 'i686' then
+    oraclejdk_rpm = "jdk-#{oraclejdk_version}-linux-i586.rpm"
+  elsif node['kernel']['machine'] == 'x86_64' then
+    oraclejdk_rpm = "jdk-#{oraclejdk_version}-linux-x64.rpm"
+  end
 
-download_url = "http://download.oracle.com/otn-pub/java/jdk/#{oraclejdk_build}/#{oraclejdk_rpm}"
+  download_url = "http://download.oracle.com/otn-pub/java/jdk/#{oraclejdk_build}/#{oraclejdk_rpm}"
 
-script "download oracle jdk" do
-  interpreter "bash"
-  user "root"
-  cwd cache_path
-  code <<-EOH
-    wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" #{download_url} -O #{oraclejdk_rpm}
-  EOH
-  not_if { File.exists?("#{cache_path}/#{oraclejdk_rpm}") }
-end
+  script "download oracle jdk" do
+    interpreter "bash"
+    user "root"
+    cwd cache_path
+    code <<-EOH
+      wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" #{download_url} -O #{oraclejdk_rpm}
+    EOH
+    not_if { File.exists?("#{cache_path}/#{oraclejdk_rpm}") }
+  end
 
-rpm_package "jdk" do
-  source "#{cache_path}/#{oraclejdk_rpm}"
-  action :install
-end
+  rpm_package "jdk" do
+    source "#{cache_path}/#{oraclejdk_rpm}"
+    action :install
+  end
 
-script "change java path" do
-  interpreter "bash"
-  user "root"
-  code <<-EOH
-    update-alternatives --set java /usr/java/#{jdk_version}/jre/bin/java
-  EOH
+  script "change java path" do
+    interpreter "bash"
+    user "root"
+    code <<-EOH
+      update-alternatives --set java /usr/java/#{jdk_version}/jre/bin/java
+    EOH
+  end
 end
